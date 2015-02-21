02c86f804ea0f5a99f0923f5f3c1f1e005d60807
Santiago Pelufo <santiagopelufo@gmail.com>
2015-02-21 19:19:09 -0200

    downloaded previas de todas las carreras. next raw html pages -> structured data

--- scrape.sh
+++ scrape.sh
@@ -1,19 +1,47 @@
 #!/bin/bash
 
-# ./wget-download.sh bedelias.edu.uy
+# Scrape bedelias.edu.uy por informacion de previas.
+#
+# Requisitos:
+#
+# 	$ grep --version
+# 	grep (GNU grep) 2.16
+#
+# 	$ bash --version
+# 	GNU bash, version 4.3.11(1)-release (x86_64-pc-linux-gnu)
+#
+# 	$ apt install html-xml-utils
+#   $ apt show html-xml-utils
+# 	Package: html-xml-utils
+# 	State: installed
+# 	Automatically installed: no
+# 	Version: 6.5-1
+# 	...
 
-# grep -oP 'href="([^"]+)"' bedelias.edu.uy/sti/facultades.html | cut -d ':' -f2 | cut -d '/' -f '3-' | grep -oP --color=none '[^"]+' > facultades.lns
 
-# while read -r l; do ./wget-download.sh "$l"; done < facultades.lns
+./wget-download.sh bedelias.edu.uy
 
-# FACDS=$(echo www{1,2,3}.bedelias.edu.uy/*/)
+grep -oP --color=none 'href=".*\K(www\d[^"]+)' bedelias.edu.uy/sti/facultades.html > facultades.lns
 
-# for f in $FACDS; do
-# 	grep -oP "parent.contenidos.location[^']+'\K([^']+)(?=')" $f/menu.planes* | while read -r lnk; do echo "${f/\/*/}""$lnk"; done
-# done > planes.lns
+while read -r l; do ./wget-download.sh "$l"; done < facultades.lns
 
-# cat planes.lns | grep -P --color=none 'mats\.sel_carr8$' > planes-materias.lns
-# cat planes.lns | grep -P --color=none 'carrs\.muestreo_carreras$' > planes-carreras.lns
-# PREVS=$(echo www{1,2,3}.bedelias.edu.uy/*/muestra)
+FACDS=$(echo www{1,2,3}.bedelias.edu.uy/*/)
+
+for f in $FACDS; do
+	grep -oP "parent.contenidos.location[^']+'\K([^']+)(?=')" $f/menu.planes* | while read -r lnk; do echo "${f/\/*/}""$lnk"; done
+done > planes.lns
+
+PREVS=$(echo www{1,2,3}.bedelias.edu.uy/*/muestra)
 
 while read -r l; do ./wget-download.sh "$l"; done < planes.lns
+
+for f in $FACDS
+do
+	echo "# $f" >> carreras.txt
+	if [[ -f "${f}muestra_prev.selcarr.html" ]]; then
+		cars=$(hxnormalize -x "${f}muestra_prev.selcarr.html" -i 0 -l 300 | hxselect option -s '\n' | grep -oP '\d+\s*-\s*\d+[^<]+' | tee -a carreras.txt | cut -d ' ' -f 1)
+		for car in $cars; do ./wget-download.sh "${f}muestra_prev.imprime?carrera=${car/-/&cicl=}&p_mat=_"; done
+	else
+		echo "# File doesn't exist: ${f}muestra_prev.selcarr.html"  >> carreras.txt
+	fi
+done
